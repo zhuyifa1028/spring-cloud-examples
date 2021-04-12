8a02349a5e829d0a34613acf12163b6b7858efec
夜语丶 <zhuyifa1028@gmail.com>
2021-04-12 19:54:03 +0800

    Eureka 服务提供方

--- spring-cloud-eureka-client/src/main/java/com/github/zhuyifa1028/EurekaClientApplication.java
+++ spring-cloud-eureka-client/src/main/java/com/github/zhuyifa1028/EurekaClientApplication.java
@@ -2,20 +2,20 @@ package com.github.zhuyifa1028;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
- * 服务注册中心
+ * 服务提供方
  *
  * @author zhuyifa
- * @version 2021-04-11
+ * @version 2021-04-12
  */
-@EnableEurekaServer
+@EnableDiscoveryClient
 @SpringBootApplication
-public class EurekaServerApplication {
+public class EurekaClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
+        SpringApplication.run(EurekaClientApplication.class, args);
     }
 
 }
